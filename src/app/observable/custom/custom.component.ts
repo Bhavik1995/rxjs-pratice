1af92c4f358419c61a0603f8cf8d8c030fe99ca5
bhavik.sejpal <bhavik.sejpal@knoxpo.com>
2023-06-26 18:41:21 +0530

    code updated

--- src/app/observable/custom/custom.component.ts
+++ src/app/observable/custom/custom.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Component({
   selector: 'app-custom',
@@ -10,6 +11,16 @@ export class CustomComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    // Ex - 01 (Manual)
+
+    const cusObs1 = new Observable((observer: Observer<string>) => {
+      observer.next('Data Emit - 1');
+    })
+
+    cusObs1.subscribe(res =>{
+      console.log(res)
+    })
   }
 
 }

3225021c38399a3b9c9a19f73bc1bb112a4a9a57
bhavik.sejpal <bhavik.sejpal@knoxpo.com>
2023-06-28 12:28:51 +0530

    pluck method code added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { OfFromComponent } from './observable/of-from/of-from.component';
 import { ToArrayComponent } from './observable/to-array/to-array.component';
 import { CustomComponent } from './observable/custom/custom.component';
 import { MapComponent } from './observable/map/map.component';
+import { PluckComponent } from './observable/pluck/pluck.component';
 
 const routes: Routes = [
   {
@@ -38,6 +39,9 @@ const routes: Routes = [
       },
       {
         path: 'map', component: MapComponent
+      },
+      {
+        path: 'pluck', component: PluckComponent
       }
     ]
   },
---
+++ src/app/observable/pluck/pluck.component.ts
@@ -0,0 +1,72 @@
+import { Component, OnInit } from '@angular/core';
+import { from, map, pluck, toArray } from 'rxjs';
+
+@Component({
+  selector: 'app-pluck',
+  templateUrl: './pluck.component.html',
+  styleUrls: ['./pluck.component.scss']
+})
+export class PluckComponent implements OnInit {
+
+
+  arrayData?: any;
+  constructor() { }
+
+  users = [
+    {
+      name: 'Bhavik',
+      skills: 'Angular',
+      job:{
+        title: 'Front-End Developer',
+        exp: '1 year'
+      }
+    },
+    {
+      name: 'Vishal',
+      skills: 'JavaScript',
+      job:{
+        title: 'JavaScript Developer',
+        exp: '3 year'
+      }
+    },
+    {
+      name: 'Prajwal',
+      skills: 'React',
+      job:{
+        title: 'React Developer',
+        exp: '2 year'
+      }
+    },
+    {
+      name: 'Aditya',
+      skills: 'TypeScript',
+      job:{
+        title: 'MERN Developer',
+        exp: '2.5 year'
+      }
+    },
+    {
+      name: 'Ashwin',
+      skills: 'HTML',
+      job:{
+        title: 'HTML Developer',
+        exp: '1 year'
+      }
+    }
+  ]
+
+  ngOnInit(): void {
+
+    from(this.users).pipe
+      (
+        pluck('name'),
+        toArray()
+      )
+      .subscribe(res => {
+        console.log(res)
+
+        this.arrayData = res;
+      })
+  }
+
+}

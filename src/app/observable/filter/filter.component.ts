c64902e7dca414854480804d94b5ad5d4c6fbd94
bhavik.sejpal <bhavik.sejpal@knoxpo.com>
2023-06-28 17:46:50 +0530

    filter operator code updated

--- src/app/observable/filter/filter.component.ts
+++ src/app/observable/filter/filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { filter, from, toArray } from 'rxjs';
 
 @Component({
   selector: 'app-filter',
@@ -7,9 +8,86 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterComponent implements OnInit {
 
+  dataArr = [
+    {
+      id: 1,
+      name: 'Bhavik',
+      gender: 'Male'
+    },
+    {
+      id: 2,
+      name: 'Vishal',
+      gender: 'Male'
+    },
+    {
+      id: 3,
+      name: 'Prajwal',
+      gender: 'Male'
+    },
+    {
+      id: 4,
+      name: 'Aditya',
+      gender: 'Male'
+    },
+    {
+      id: 5,
+      name: 'Ashwin',
+      gender: 'Male'
+    },
+    {
+      id: 5,
+      name: 'Vidhi',
+      gender: 'Female'
+    },
+    {
+      id: 6,
+      name: 'Shweta',
+      gender: 'Female'
+    }
+  ]
+
+  data?: any;
+  data2?: any;
+  data3?: any;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    const source = from(this.dataArr)
+
+    // Ex - 01 - Filter by length
+
+    source.pipe(
+      filter(member => member.name.length > 6),
+      toArray()
+    ).subscribe(res => {
+      //  
+
+      this.data = res;
+    })
+
+    // Ex - 02 - Filter by gender
+
+    source.pipe(
+      filter(member=> member.gender == 'Female'),
+      toArray()
+    ).subscribe(res => {
+      // console.log(res);
+
+      this.data2 = res;
+    })
+
+    // Ex - 03 - Length Item
+
+    source.pipe(
+      filter(member=> member.id <= 4),
+      toArray()
+    ).subscribe(res => {
+      console.log(res);
+
+      this.data3 = res;
+    })
   }
 
 }

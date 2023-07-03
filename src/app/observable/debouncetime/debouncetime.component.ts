9a626ce36fd0d13be33f57bb986686852f8e3bc7
bhavik.sejpal <bhavik.sejpal@knoxpo.com>
2023-07-03 12:03:58 +0530

    distnict until changed updated

--- src/app/observable/debouncetime/debouncetime.component.ts
+++ src/app/observable/debouncetime/debouncetime.component.ts
@@ -1,6 +1,6 @@
 import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { LoadingBarService } from '@ngx-loading-bar/core';
-import { debounceTime, fromEvent, map } from 'rxjs';
+import { debounceTime, distinctUntilChanged, fromEvent, map } from 'rxjs';
 
 @Component({
   selector: 'app-debouncetime',
@@ -17,6 +17,10 @@ export class DebouncetimeComponent implements OnInit, AfterViewInit {
 
   @ViewChild('myInput') myInput?: ElementRef;
   requestedData = null;
+
+
+  @ViewChild('myInput2') myInput2?: ElementRef;
+  requestedData2 = null;
   constructor(private loadingBar: LoadingBarService) { }
 
   ngOnInit(): void {
@@ -32,6 +36,8 @@ export class DebouncetimeComponent implements OnInit, AfterViewInit {
   }
 
   ngAfterViewInit(): void {
+
+    // Ex - 01 Debounce Time 
     
     const searchTerm = fromEvent<any>(this.myInput?.nativeElement,'keyup').pipe(
       map(event => event.target.value),
@@ -48,6 +54,26 @@ export class DebouncetimeComponent implements OnInit, AfterViewInit {
         this.stopLoading();
       }, 2000)
     }) 
+
+    // Ex - 02 Distinct UntilChanged
+
+    const searchTerm2 = fromEvent<any>(this.myInput2?.nativeElement,'keyup').pipe(
+      map(event => event.target.value),
+      debounceTime(1000),
+      distinctUntilChanged()
+    )
+
+    searchTerm2.subscribe(res =>{ 
+      console.log(res)  
+      this.requestedData2 = res;
+      this.startLoading();
+
+      setTimeout(()=>{
+        this.requestedData2 = null;
+        this.stopLoading();
+      }, 2000)
+    }) 
+
   }
 
 }

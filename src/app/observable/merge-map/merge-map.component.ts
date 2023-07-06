92b04ddd7863758ed5f292cf1def20cc2b69e48b
bhavik.sejpal <bhavik.sejpal@knoxpo.com>
2023-07-06 18:02:08 +0530

    merge map operator added

--- src/app/observable/merge-map/merge-map.component.ts
+++ src/app/observable/merge-map/merge-map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { from, map, mergeAll, mergeMap, of } from 'rxjs';
+import { DesignUtilityService } from 'src/app/services/design-utility.service';
 
 @Component({
   selector: 'app-merge-map',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MergeMapComponent implements OnInit {
 
-  constructor() { }
+  constructor(private designUtility: DesignUtilityService) { }
+
+  getData(data: any){
+    return of(data + ' Video Uploaded') 
+  } 
 
   ngOnInit(): void {
-  }
 
-}
+    const source = from(['Tech','Comedy','News']);
+
+  //  Ex - 01 | Map
+
+  source.pipe(
+    map(res => this.getData(res))
+  ).subscribe(res=> res.subscribe(res2 =>{
+    console.log('Map => '+res2);
+
+    this.designUtility.print(res2,'elContainer');
+  }))
+
+    //  Ex - 02 | Map + MergeAll
+
+    source.pipe(
+      map(res => this.getData(res)),
+      mergeAll()
+    ).subscribe(res=> {
+      console.log('Map + MergeAll => ',res);
+      this.designUtility.print(res,'elContainer2');
+    })
+
+
+    //  Ex - 03 | MergeMap
+
+    source.pipe(
+      mergeMap(res => this.getData(res)),
+    ).subscribe(res=> {
+      console.log('MergeMap => ',res);
+      this.designUtility.print(res,'elContainer3');
+    })
+    }
+  }

2abe0471f021ddd9d95d62d1c62b4daf79721cb6
bhavik.sejpal <bhavik.sejpal@knoxpo.com>
2023-07-07 11:11:40 +0530

    concat map operator code updated

--- src/app/observable/concat-map/concat-map.component.ts
+++ src/app/observable/concat-map/concat-map.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { concatAll, concatMap, delay, from, map, mergeMap, of } from 'rxjs';
+import { DesignUtilityService } from 'src/app/services/design-utility.service';
 
 @Component({
   selector: 'app-concat-map',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConcatMapComponent implements OnInit {
 
-  constructor() { }
+  constructor(private designUtlity: DesignUtilityService) { }
+
+  getData(data: any) {
+    return of(data + ' Video Uploaded')
+  }
 
   ngOnInit(): void {
+
+    const source = from(['Tech', 'Comedy', 'News']).pipe(delay(2000))
+
+    // Ex - 01 | Map
+
+    source.pipe(
+      map(res => this.getData(res))
+    )
+      .subscribe(res => res.subscribe(res2 => {
+        this.designUtlity.print(res2, 'elContainer')
+      }))
+
+    // Ex - 02 | MergeMap
+
+    source.pipe(
+      mergeMap(res => this.getData(res))
+    )
+      .subscribe(res => {
+        this.designUtlity.print(res, 'elContainer2')
+      })
+
+    // Ex - 03 | ConcatMap
+
+    source.pipe(
+      concatMap(res => this.getData(res)),
+    )
+      .subscribe(res => {
+        this.designUtlity.print(res, 'elContainer3')
+      })
   }
 
 }

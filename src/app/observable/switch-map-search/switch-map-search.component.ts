3ab3d0f26b6c73681bc9518bfd486bb8c663a481
bhavik.sejpal <bhavik.sejpal@knoxpo.com>
2023-07-12 12:01:39 +0530

    switch map component added

--- src/app/observable/switch-map-search/switch-map-search.component.ts
+++ src/app/observable/switch-map-search/switch-map-search.component.ts
@@ -1,17 +1,31 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, map, pluck } from 'rxjs';
 
 @Component({
   selector: 'app-switch-map-search',
   templateUrl: './switch-map-search.component.html',
   styleUrls: ['./switch-map-search.component.scss']
 })
-export class SwitchMapSearchComponent implements OnInit {
+export class SwitchMapSearchComponent implements AfterViewInit {
 
   @ViewChild('searchForm') searchForm?: NgForm
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    
+    const formValue= this.searchForm?.valueChanges;
+    formValue?.pipe(
+      map(data=> data),
+      debounceTime(500),
+      distinctUntilChanged() 
+     )
+     .subscribe(res =>{
+       console.log(res)
+     })
+
+
   }
 
 }
